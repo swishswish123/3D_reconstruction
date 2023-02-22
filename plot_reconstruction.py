4078f4620aaafd62dcb740ad3d77560483b9ec16
Aure <swishswish123@gmail.com>
2023-02-22 11:02:48 +0000

    ref: plot_reconstruction clearing

---
+++ plot_reconstruction.py
@@ -0,0 +1,82 @@
+import open3d as o3d
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+import pandas as pd 
+import plotly.graph_objs as go
+from pathlib import Path
+
+
+if __name__=='__main__':
+    
+    ########################## PARAMS ###################################
+
+    # method reconstruction was performed with:
+    # sksurgery/ online/ prince /method_3 
+    method = 'method_3'
+    # folder of type of video
+    # random / phantom / EM_tracker_calib
+    type='random' 
+    # folder where image folder located
+    # RANDOM, UNDISTORTED: arrow / brain  / checkerboard_test_calibrated / gloves / 
+    # RANDOM, Distorted: books / points / spinal_section / spinal_section_pink
+    # EM_TRACKING_CALIB testing_points /testing_lines
+    # RANDOM, UNDISTORTED WITH MAC: mac_camera
+    folder = 'mac_camera'
+
+    ########################## LOADING ALL ###################################
+
+    project_path = Path(__file__).parent.resolve()
+    reconstruction_output = f'{project_path}/reconstructions/{method}/{type}/{folder}'
+    # 3D points
+    x = np.load(f'{reconstruction_output}/points.npy')
+    # colours
+    c = np.load(f'{reconstruction_output}/colors.npy')
+    
+    # database of 3D points and colors
+    df = pd.DataFrame({'X':x[:,0], 'Y':x[:,1], 'Z':x[:,2], 'R':c[:,0], 'G':c[:,1], 'B':c[:,2]})
+
+    ########################## REMOVE OUTLIERS ###################################
+    
+    # find all rows with any col values larger than max_num
+    max_num = 100
+    min_num = -100
+    df = df.drop(df[ (df['X'] > max_num) | (df['Y'] > max_num) | (df['Z'] > max_num) ].index, inplace=False)
+    df = df.drop(df[ (df['X'] < min_num) | (df['Y'] < min_num) | (df['Z'] < min_num) ].index, inplace=False)
+
+    ########################## PLOTTING ###################################
+
+    # plot scatter graph with colors
+    trace = go.Scatter3d(x=df.X,
+                      y=df.Y,
+                      z=df.Z,
+                      mode='markers',
+                      marker=dict(size=3,
+                                  color=['rgb({},{},{})'.format(r,g,b) for r,g,b in zip(df.R.values, df.G.values, df.B.values)],
+                                  opacity=0.9,)
+                                  )
+
+    data = [trace]
+    layout = go.Layout(margin=dict(l=0,
+                                    r=0,
+                                    b=0,
+                                    t=0),
+                                    scene=dict(
+                 aspectmode='data'
+         ))
+
+    fig = go.Figure(data=data, layout=layout)
+    fig.show()
+
+    # MATPLOTLIB METHOD
+    '''
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+   
+    #ax = fig.gca(projection='3d')
+    ax.set_aspect("equal")
+    ax.scatter(df.X,df.Y,df.Z, c=df[['R', 'G', 'B']]/255.0,s=10, marker='.')
+
+    plt.savefig('3D_scatter_surface.png')
+    print('done')
+    '''
---
+++ plot_reconstruction_o3d.py
@@ -0,0 +1,48 @@
+import open3d as o3d
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+import pandas as pd 
+import plotly.graph_objs as go
+from pathlib import Path
+
+
+if __name__=='__main__':
+    ########################## PARAMS ###################################
+
+    method = 'online'
+    type='random'
+    folder = 'books'
+
+    ########################## LOADING ALL ###################################
+
+    project_path = Path(__file__).parent.resolve()
+    # where 3D points and colors located
+    reconstruction_output = f'{project_path}/reconstructions/{method}/{type}/{folder}'
+    # 3D points
+    xyz = np.load(f'{reconstruction_output}/points.npy')
+    # colors
+    c = np.load(f'{reconstruction_output}/colors.npy')
+    
+    ########################## PLOTTING ###################################
+
+    # creating pointcloud with colors, saving it
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(xyz)
+    pcd.colors = o3d.utility.Vector3dVector(c)
+    o3d.io.write_point_cloud("./sync.ply", pcd)
+
+    # visualizing point cloud ->>>>> doesn't work on mac m1
+    pcd_load = o3d.io.read_point_cloud("./sync.ply")
+        # convert Open3D.o3d.geometry.PointCloud to numpy array
+    xyz_load = np.asarray(pcd_load.points)
+    print('xyz_load')
+    print(xyz_load)
+
+    # save scatter as an image (change [0,1] range to [0,255] range with uint8 type)
+    img = o3d.geometry.Image((xyz * 255).astype(np.uint8))
+    o3d.io.write_image(".sync.png", img) 
+    o3d.visualization.draw_geometries([img])
+    o3d.visualization.draw_geometries([pcd])
+    
+    
