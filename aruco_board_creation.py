d4285c024c8360c5ed599a0b696f9394a28e586c
Aure <swishswish123@gmail.com>
2023-03-17 15:35:01 +0000

    fix: aruco creation and detection opencv update
    updated the aruco creation and detection with opencv-contrib latest version as couldn't get it to work with opencv

--- aruco_board_creation.py
+++ aruco_board_creation.py
@@ -4,7 +4,7 @@ bug: can't figure out how to make image same dimension for printing
 '''
 import cv2
 from pathlib import Path
-
+from cv2 import aruco_Board
 
 if __name__=='__main__':
     # https://stackoverflow.com/questions/59789491/using-board-create-for-aruco-markers-leads-to-error-due-to-objpoints-not-in-the
@@ -16,24 +16,29 @@ if __name__=='__main__':
     # Settings for the marker
     markers_w = 5 # Number of markers in the X direction.
     markers_h = 7 # Number of markers in the y direction.
-    marker_length = 0.01 # length of aruco marker (m)
+    marker_length = 0.04 # length of aruco marker (m)
     marker_separation = 0.01 # separation between markers (m)
     ar = cv2.aruco.DICT_4X4_50 # aruco dictionary we will use
-    aruco_dict = cv2.aruco.Dictionary_get(ar) # dictionary of markers provided
+    aruco_dict = cv2.aruco.getPredefinedDictionary(ar) # dictionary of markers provided
 
     # creat an aruco Board (The ids in ascending order starting on 0)
-    grid_board = cv2.aruco.GridBoard_create(markers_w,
+    grid_board = cv2.aruco.GridBoard((markers_w, markers_h),
+                                    marker_length,
+                                    marker_separation,
+                                    aruco_dict)
+    '''
+    grid_board = cv2.aruco_Board.GridBoard_create(markers_w,
                                             markers_h,
                                             marker_length,
                                             marker_separation,
                                             aruco_dict)
-
+    '''
     # now we want to print this board so we create an image:
     # convert to image
-    img_size = (2400 , 3450)
-    img_margin = 100 # margin in pixels, so none of the markers are touching the image border. In this case the margin is 10.
-    # img = grid_board.draw(img_size, img_margin, 10)
-    img = cv2.aruco.Board.generateImage(img_size)
+    img_size = (2400, 3450)
+    margin_size = 100 # minimum margins (in pixels) so that markers don't touch the board in the output image
+    border_bits = 100 #width of the marker borders
+    img = grid_board.generateImage(img_size, margin_size, border_bits)
 
     cv2.imwrite("aruco_board.png", img)
 
--- record_video_and_pose_aruco.py
+++ record_video_and_pose_aruco.py
@@ -1,16 +1,17 @@
+import copy
+
 import cv2
 import numpy as np
 from pathlib import Path
-import os 
+import os
+
 
 def aruco_display(corners, ids, rejected, image, rvecs, tvecs):
-
     if len(corners) > 0:
-		# ids of aruco markers detected
+        # ids of aruco markers detected
         ids = ids.flatten()
 
         for (markerCorner, markerID) in zip(corners, ids):
-
             corners = markerCorner.reshape((4, 2))
             (topLeft, topRight, bottomRight, bottomLeft) = corners
 
@@ -28,76 +29,62 @@ def aruco_display(corners, ids, rejected, image, rvecs, tvecs):
             cY = int((topLeft[1] + bottomRight[1]) / 2.0)
             cv2.circle(image, (cX, cY), 4, (0, 0, 255), -1)
 
-            cv2.putText(image, str(markerID),(topLeft[0], topLeft[1] - 10), cv2.FONT_HERSHEY_SIMPLEX,
-                0.5, (0, 0, 255), 3)
-            print("[Inference] ArUco marker ID: {}".format(markerID))
+            cv2.putText(image, str(markerID), (topLeft[0], topLeft[1] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.5, (0, 0, 255), 3)
+            # print("ArUco marker ID: {}".format(markerID))
 
         avg_z = np.array(tvecs).mean(axis=0).squeeze()
-        print(avg_z)
 
         # WRITING DISTANCE
-        cv2.putText(image, str(int(avg_z[2])),(50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 3)	
- 
+        cv2.putText(image, str(int(avg_z[2])), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 3)
+
     return image
 
 
-def single_marker_pose_estimation(frame, aruco_dict_type, matrix_coefficients, distortion_coefficients):
+def aruco_single_pose_estimation(frame, aruco_dict, intrinsics, distortion):
 
-    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.aruco_dict = aruco_dict_type
-    parameters = cv2.aruco.DetectorParameters_create()
-
-
-    corners, ids, rejected_img_points = cv2.aruco.detectMarkers(gray, cv2.aruco_dict,parameters=parameters,
-        cameraMatrix=matrix_coefficients,
-        distCoeff=distortion_coefficients)
-
+    annotated_frame = copy.deepcopy(frame)
+    parameters = cv2.aruco.DetectorParameters()
+    corners, ids, rejected_img_points = cv2.aruco.detectMarkers(annotated_frame, aruco_dict, parameters=parameters)
 
     rvecs = []
-    tvecs = []   
+    tvecs = []
     if len(corners) > 0:
         for i in range(0, len(ids)):
-           
-            rvec, tvec, markerPoints = cv2.aruco.estimatePoseSingleMarkers(corners[i], MARKER_LENGTH, matrix_coefficients,
-                                                                       distortion_coefficients)
-            
-            cv2.aruco.drawDetectedMarkers(frame, corners) 
+            rvec, tvec, markerPoints = cv2.aruco.estimatePoseSingleMarkers(corners[i],
+                                                                           MARKER_LENGTH, intrinsics,
+                                                                           distortion)
 
-            cv2.aruco.drawAxis(frame, matrix_coefficients, distortion_coefficients, rvec, tvec, 0.01)  
+            # draw detected markers corners and borders
+            cv2.aruco.drawDetectedMarkers(annotated_frame, corners)
+
+            # drawing axis of aruco
+            cv2.drawFrameAxes(annotated_frame, intrinsics, distortion, rvec, tvec, length=.2, thickness=3)
+
             rvecs.append(rvec)
             tvecs.append(tvec)
 
-    frame = aruco_display(corners, ids, rejected_img_points, frame, rvecs, tvecs)
-    
-    tvecs = np.array(tvecs).mean(axis=0).squeeze()
-    rvecs = np.array(rvecs).mean(axis=0).squeeze()
+    annotated_frame = aruco_display(corners, ids, rejected_img_points, annotated_frame, rvecs, tvecs)
 
-    return frame, rvecs, tvecs
+    return annotated_frame, rvecs, tvecs
 
 
-def aruco_board_pose_estimation(frame, aruco_dict_type, matrix_coefficients, distortion_coefficients):
+def aruco_board_pose_estimation(frame, aruco_dict, intrinsics, distortion):
     # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    markers_w = 5 # Number of markers in the X direction.
-    markers_h = 7 # Number of markers in the y direction.
-    marker_length = 0.04 # length of aruco marker (m)
-    marker_separation = 0.01 # separation between markers (m)
-    ar = cv2.aruco.DICT_4X4_50 # aruco dictionary we will use
-    aruco_dict = cv2.aruco.Dictionary_get(ar) # dictionary of markers provided
+    markers_w = 5  # Number of markers in the X direction.
+    markers_h = 7  # Number of markers in the y direction.
+    marker_length = 0.04  # length of aruco marker (m)
+    marker_separation = 0.01  # separation between markers (m)
 
     # creat an aruco Board (The ids in ascending order starting on 0)
-    grid_board = cv2.aruco.GridBoard_create(markers_w,
-                                            markers_h,
-                                            marker_length,
-                                            marker_separation,
-                                            aruco_dict)
+    grid_board = cv2.aruco.GridBoard((markers_w, markers_h),
+                                     marker_length,
+                                     marker_separation,
+                                     aruco_dict)
 
+    parameters = cv2.aruco.DetectorParameters()
 
-    cv2.aruco_dict = aruco_dict_type
-    parameters = cv2.aruco.DetectorParameters_create()
-
-    corners, ids, rejected_img_points = cv2.aruco.detectMarkers(frame, cv2.aruco_dict, parameters=parameters,
-                                                                cameraMatrix=matrix_coefficients,
-                                                                distCoeff=distortion_coefficients)
+    corners, ids, rejected_img_points = cv2.aruco.detectMarkers(frame, aruco_dict, parameters=parameters)
     rvec = None
     tvec = None
 
@@ -111,7 +98,8 @@ def aruco_board_pose_estimation(frame, aruco_dict_type, matrix_coefficients, dis
             # draw detected markers corners
             cv2.aruco.drawDetectedMarkers(frame, corners)
             # drawing axis of aruco marker board
-            cv2.aruco.drawAxis(frame, intrinsics, distortion, rvec, tvec, 0.05)
+            # cv2.aruco.drawAxis(frame, intrinsics, distortion, rvec, tvec, 0.05)
+            cv2.drawFrameAxes(frame, intrinsics, distortion, rvec, tvec, 0.05)
 
             # adding as text the distance from camera to aruco marker (z coord of tvec)
             cv2.putText(frame, str(int(tvec[2])), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 3)
@@ -122,40 +110,40 @@ def aruco_board_pose_estimation(frame, aruco_dict_type, matrix_coefficients, dis
     return frame, rvec, tvec
 
 
-def save_frame(img,frame_num, intrinsics, distortion):
+def save_frame(img, frame_num, intrinsics, distortion):
     # undistort
     dst = cv2.undistort(img, intrinsics, distortion, None, intrinsics)
 
     # undistorted
-    cv2.imwrite('{}/images/{:08d}.png'.format(save_folder,frame_num), img) #save_folder, i
+    cv2.imwrite('{}/images/{:08d}.png'.format(save_folder, frame_num), img)  # save_folder, i
     # distorted
-    cv2.imwrite('{}/undistorted/{:08d}.png'.format(save_folder,frame_num), dst) #save_folder, i
+    cv2.imwrite('{}/undistorted/{:08d}.png'.format(save_folder, frame_num), dst)  # save_folder, i
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     BOARD = True
     project_path = Path(__file__).parent.resolve()
     # folder will be structured as follows:
     # assets/type/folder/images
 
-    type='aruco' # random / phantom / EM_tracker_calib / tests
-    folder = 'shelves_video_2'
+    type = 'aruco'  # random / phantom / EM_tracker_calib / tests
+    folder = 'aruco_test'
 
     save_folder = f'{project_path}/assets/{type}/{folder}'
 
-    MARKER_LENGTH = 30 # mm
-    RECORD_ALL=False
-    FRAME_RATE=1
+    MARKER_LENGTH = 30  # mm
+    RECORD_ALL = False
+    FRAME_RATE = 1
     # CREATING FOLDERS WHERE TO SAVE FRAMES AND TRACKING INFO
     if not os.path.isdir(f'{save_folder}/images'):
-        os.makedirs(f'{save_folder}/images')  
-        os.makedirs(f'{save_folder}/undistorted')  
+        os.makedirs(f'{save_folder}/images')
+        os.makedirs(f'{save_folder}/undistorted')
 
     intrinsics = np.loadtxt(f'{project_path}/calibration/mac_calibration/intrinsics.txt')
     distortion = np.loadtxt(f'{project_path}/calibration/mac_calibration/distortion.txt')
 
-    arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
-    arucoParams = cv2.aruco.DetectorParameters_create()
+    ar = cv2.aruco.DICT_4X4_50  # aruco dictionary we will use
+    aruco_dict = cv2.aruco.getPredefinedDictionary(ar)  # dictionary of markers provided
 
     cap = cv2.VideoCapture(0)
 
@@ -171,43 +159,37 @@ if __name__=='__main__':
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         if BOARD:
-            output, rvecs, tvecs  = aruco_board_pose_estimation(frame, arucoDict, intrinsics, distortion)
+            output, rvecs, tvecs = aruco_board_pose_estimation(frame, aruco_dict, intrinsics, distortion)
         else:
-            continue
+            output, rvecs, tvecs = aruco_single_pose_estimation(frame, aruco_dict, intrinsics, distortion)
+
         cv2.imshow('Estimated Pose', output)
 
         key = cv2.waitKey(1) & 0xFF
         if key == ord('c'):
-            print('recording frame')
-            save_frame(frame,frame_num, intrinsics, distortion)
+            save_frame(frame, frame_num, intrinsics, distortion)
             rvecs_all.append(rvecs)
             tvecs_all.append(tvecs)
-        
+
         if RECORD_ALL:
-            if frame_num%FRAME_RATE==0:
+            if frame_num % FRAME_RATE == 0:
                 if np.isnan(rvecs).all() or np.isnan(tvecs).all():
                     continue
                 else:
-                    print('recording frame')
-                    save_frame(frame,frame_num, intrinsics, distortion)
+                    save_frame(frame, frame_num, intrinsics, distortion)
                     rvecs_all.append(rvecs)
                     tvecs_all.append(tvecs)
 
         frame_num += 1
         if key == ord("q"):
             break
-        
 
-    rvecs_all = np.array(rvecs_all).squeeze() #.mean(axis=0)
-    tvecs_all = np.array(tvecs_all).squeeze() #.mean(axis=0)
+    rvecs_all = np.array(rvecs_all).squeeze()  # .mean(axis=0)
+    tvecs_all = np.array(tvecs_all).squeeze()  # .mean(axis=0)
 
     # SAVE RT VECTORS AND TIMES
-    np.save(f'{save_folder}/rvecs',np.array(rvecs_all))
-    np.save(f'{save_folder}/tvecs',np.array(tvecs_all))
-
-        
+    np.save(f'{save_folder}/rvecs', np.array(rvecs_all))
+    np.save(f'{save_folder}/tvecs', np.array(tvecs_all))
 
 cv2.destroyAllWindows()
 cap.release()
-
-
